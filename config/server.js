02f7b07c2caf8cceecb66a0d0b3c3e8bfdeeb1c5
malek <malek@cosavostra.com>
2022-01-08 18:58:27 +0100

    init back

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cf5782c89345769c6ddd7dd2ec64f14f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd32f76b6-1ff7-4519-80c6-b7c5054c6257'
+};
